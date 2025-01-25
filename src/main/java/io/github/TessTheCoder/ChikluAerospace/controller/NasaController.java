a2d225aec74177b5862d934a59432d7c90739cde
Dinesh Solanki <15937452+DineshSolanki@users.noreply.github.com>
2025-01-25 17:36:14 +0530

    add NASA API configuration and update controller methods

--- src/main/java/io/github/TessTheCoder/ChikluAerospace/controller/NasaController.java
+++ src/main/java/io/github/TessTheCoder/ChikluAerospace/controller/NasaController.java
@@ -3,49 +3,68 @@ package io.github.TessTheCoder.ChikluAerospace.controller;
 import io.github.TessTheCoder.ChikluAerospace.dto.NeoResponse;
 import io.github.TessTheCoder.ChikluAerospace.dto.PictureResponse;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.actuate.endpoint.web.Link;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.util.UriComponentsBuilder;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.LinkedHashMap;
 
 @Controller
 class NasaController {
+    public static final String PICTURE = "picture";
+    public static final String API_KEY = "api_key";
     private final RestTemplate restTemplate;
 
     public NasaController(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
+    @Value("${nasa.api.base}")
+    private String nasaBaseUrl;
 
-    private static final String NASA_APOD_URL = "https://api.nasa.gov/planetary/apod";
-    private static final String NASA_NEO_URL = "https://api.nasa.gov/neo/rest/v1/feed";
+    private static final String NASA_APOD_URL = "planetary/apod";
+    private static final String NASA_NEO_URL = "neo/rest/v1/feed";
 
     @Value("${nasa.api.key}")
-    private String API_KEY;
+    private String apiKey;
 
     @GetMapping("/picture")
-    public ModelAndView getAstronomyPictureOfTheDay( Model model) {
-        String currentDate = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
-        String url = String.format("%s?api_key=%s&date=%s" , NASA_APOD_URL, API_KEY, currentDate);
+    public ModelAndView getAstronomyPictureOfTheDay(@RequestParam(required = false) LocalDate date, Model model) {
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromUriString(nasaBaseUrl)
+                .path(NASA_APOD_URL)
+                .queryParam(API_KEY, apiKey)
+                .queryParam("date", getDateIfRequired(date));
+
+        String url = uriComponentsBuilder.toUriString();
+
         PictureResponse response = restTemplate.getForObject(url, PictureResponse.class);
-        model.addAttribute("picture", response);
-        model.addAttribute("url", response.getUrl());
-        return new ModelAndView("picture");
+        model.addAttribute(PICTURE, response);
+        return new ModelAndView(PICTURE);
     }
 
     @GetMapping("/neo")
-    public ModelAndView getNearEarthObjects( Model model) {
-        LocalDate currentDate = LocalDate.now();
-        String formattedDate = currentDate.format(DateTimeFormatter.ISO_DATE);
-        String url = String.format("%s?api_key=%s&start_date=%s&end_date=%s", NASA_NEO_URL, API_KEY, formattedDate, formattedDate);
+    public ModelAndView getNearEarthObjects(@RequestParam(required = false) LocalDate date, Model model) {
+        String formattedDate = getDateIfRequired(date);
+        UriComponentsBuilder uriComponentsBuilder = UriComponentsBuilder.fromUriString(nasaBaseUrl)
+                .path(NASA_NEO_URL)
+                .queryParam(API_KEY, apiKey)
+                .queryParam("start_date", formattedDate)
+                .queryParam("end_date", formattedDate);
+
+        String url = uriComponentsBuilder.toUriString();
         NeoResponse response = restTemplate.getForObject(url, NeoResponse.class);
         model.addAttribute("neoResponse", response);
-      //  model.addAttribute("neo", response.getneo());
         return new ModelAndView("neo");
+    }
+
+    private static String getDateIfRequired(LocalDate date) {
+        if (date == null) {
+            date = LocalDate.now();
+        }
+        return date.format(DateTimeFormatter.ISO_LOCAL_DATE);
     }
 }
